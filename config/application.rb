2370ba34fb8004af87ed3906aef1924af4e3adfb
SoraSuzuki <sora1214baseba4@gmail.com>
2021-03-05 12:24:44 +0900

    git miss again

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    
     config.load_defaults 5.2
 
     
